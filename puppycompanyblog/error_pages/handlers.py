e70d62a3bd38988f58db116ac2878bd089614f0a
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-28 11:26:48 +0200

    Add more errors handlers and templates

--- puppycompanyblog/error_pages/handlers.py
+++ puppycompanyblog/error_pages/handlers.py
@@ -5,3 +5,8 @@ error_pages = Blueprint('error_pages', __name__)
 @error_pages.app_errorhandler(404)
 def error_404(error):
     return render_template('error_pages/404.html'), 404 #we are returning here a tuple
+
+
+@error_pages.app_errorhandler(403)
+def error_434(error):
+    return render_template('error_pages/403.html'), 403
