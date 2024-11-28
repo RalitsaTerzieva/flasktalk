0622ea553b40c837886711d91be27fda0aa409df
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-28 17:01:33 +0200

    Add Login form

---
+++ puppycompanyblog/users/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileAllowed, FileField
+from flask_login import current_user
+from puppycompanyblog.models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
