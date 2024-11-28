cbe3e0246d24e00f7d4f96f81bee406a6edc8ec3
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-28 16:24:36 +0200

    Add models

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
+login_manager = LoginManager()
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,6 +17,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
 
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handlers import error_pages
--- puppycompanyblog/models.py
+++ puppycompanyblog/models.py
@@ -1,8 +1,49 @@
-from puppycompanyblog import db
+from puppycompanyblog import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime, timezone
 
-class User():
-    pass
+
+@login_manager.user_loader #buildin decorator from the login manager
+def load_user(user_id):
+    return db.session.get(User, user_id)
+
+class User(db.Model, UserMixin):
+    
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    profile_image = db.Column(db.String(64), nullable=False, default="default_profile.png")
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('BlogPost', backref="author", lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f"Username is {self.usrname}"
 
 
-class BlogPost():
-    pass
+class BlogPost(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    users = db.relationship(User, )
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Post ID: {self.id} -- date: {self.date} -- title: {self.title}"
