73fe71568820b7461ee85b9b9fae1352b9c681d0
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-29 13:52:53 +0200

    Add register view and template

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -7,3 +7,34 @@ from puppycompanyblog.users.picture_handler import add_profile_pic
 
 
 users = Blueprint('users', __name__)
+
+
+@users.route('/register', methods=['GET', 'POST'])
+def register():
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user:
+            flash('Email already registered!')
+            return redirect(url_for('users.register'))
+        
+        new_user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Thanks for registration!')
+
+        return redirect(url_for('users.login'))
+    return render_template('register.html', form=form)
+
+
+@users.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You logged out!')
+    return redirect(url_for('core.index'))
