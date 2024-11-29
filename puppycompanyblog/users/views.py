5c6bcbd203da1d4db908be9c2c28bc02a78a9558
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-29 14:01:48 +0200

    Add login view and template

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -32,6 +32,31 @@ def register():
     return render_template('register.html', form=form)
 
 
+@users.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            flash("Logged in Successfully!")
+
+            next = request.args.get('next')  # Get the next URL the user intended to access before login.
+            
+            # Validate 'next' or default to home page
+            if next is None or next[0] != '/':
+                next = url_for('core.index')
+
+            return redirect(next)
+        
+        flash("Login Unsuccessful. Please check email and password.")
+        return render_template('login.html', form=form)
+
+    return render_template('login.html', form=form)
+
+
 @users.route('/logout')
 @login_required
 def logout():
