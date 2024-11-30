4e57326601c4d296f1dcb60d12a1d19e70619c0b
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-30 16:35:40 +0200

    Add blog post view logic

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -23,3 +23,9 @@ def create_post():
         return redirect(url_for('core.index'))
     
     return render_template('create_post.html', form=form)
+
+@blog_posts.route('/<int:blog_post_id>')
+def blog_post(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    return render_template('blog_post.html', title=blog_post.title, date=blog_post.date, post=blog_post)
